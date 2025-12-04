e37f785b4d5743c29b5630bc9e34727a78908961
abdulrahman-elzayat <abdoelzayat037@gmail.com>
2025-12-04 07:50:05 +0200

    Update solve tasl 1.cs

--- ConsoleApp2/solve tasl 1.cs
+++ ConsoleApp2/solve tasl 1.cs
@@ -11,7 +11,7 @@
             Console.WriteLine("price per small room 25$");
             Console.WriteLine("price per large room 35$");
             Console.WriteLine($"cost:{(a * 25) + (b * 35)}$");
-            Console.WriteLine($"tax:{((a * 25) + (b * 35)) * (0.06)}$g");
+            Console.WriteLine($"tax:{((a * 25) + (b * 35)) * (0.06)}$");
             Console.WriteLine($"total estimate:{((a * 25) + (b * 35)) * (0.06) + ((a * 25) + (b * 35))}$");
             Console.WriteLine("this estimate is valid 30 days");
         }
